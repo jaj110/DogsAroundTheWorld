4ceb12bc03c9a3db97f533eba955bab9a2acb3e7
jaj110 <jaj110@pitt.edu>
2017-07-31 15:27:40 -0400

    Add files via upload
    Dividing the initial DATW html page into its css and javascript components

---
+++ app.js
@@ -0,0 +1,198 @@
+
+            function frenchDog(){
+                $(function () {
+                $("#dialog").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow").click(function () {
+                $('#dialog').dialog('open');
+                });
+                });
+            }
+            function  germanDog(){
+                $(function () {
+                $("#dialog1").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow1").click(function () {
+                $('#dialog1').dialog('open');
+                });
+                });
+            }
+            function  canadianDog(){
+                $(function () {
+                $("#dialog2").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow2").click(function () {
+                $('#dialog2').dialog('open');
+                });
+                });
+            }
+            function  japaneseDog(){
+                $(function () {
+                $("#dialog3").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow3").click(function () {
+                $('#dialog3').dialog('open');
+                });
+                });
+            }
+            function  mexicanDog(){
+                $(function () {
+                $("#dialog4").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow4").click(function () {
+                $('#dialog4').dialog('open');
+                });
+                });
+            }
+            function  englishDog(){
+                $(function () {
+                $("#dialog5").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow5").click(function () {
+                $('#dialog5').dialog('open');
+                });
+                });
+            }
+            function  egyptDog(){
+                $(function () {
+                $("#dialog6").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow6").click(function () {
+                $('#dialog6').dialog('open');
+                });
+                });
+            }
+            function  brazilDog(){
+                $(function () {
+                $("#dialog7").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow7").click(function () {
+                $('#dialog7').dialog('open');
+                });
+                });
+            }
+            function  vaticanDog(){
+                $(function () {
+                $("#dialog8").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow8").click(function () {
+                $('#dialog8').dialog('open');
+                });
+                });
+            }
+            function  greekDog(){
+                $(function () {
+                $("#dialog9").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow9").click(function () {
+                $('#dialog9').dialog('open');
+                });
+                });
+            }
+            function  dutchDog(){
+                $(function () {
+                $("#dialog10").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow10").click(function () {
+                $('#dialog10').dialog('open');
+                });
+                });
+            }
+            function  irishDog(){
+                $(function () {
+                $("#dialog11").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow11").click(function () {
+                $('#dialog11').dialog('open');
+                });
+                });
+            }
+            function  norwayDog(){
+                $(function () {
+                $("#dialog12").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow12").click(function () {
+                $('#dialog12').dialog('open');
+                });
+                });
+            }
+            function  usaDog(){
+                $(function () {
+                $("#dialog13").dialog({
+                modal: true,
+                autoOpen: false,
+                title: "WOOF!",
+                width: 1400,
+                height: 900
+                });
+                $("#btnShow13").click(function () {
+                $('#dialog13').dialog('open');
+                });
+                });
+            }
+       
